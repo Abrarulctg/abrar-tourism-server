1053a71b8fc57c3129ca5637aeaff5d093b1c60d
Abrarulctg <jubairctg99@gmail.com>
2024-04-29 01:43:49 +0600

    Added Find method for findout the spot using ID

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 //6e9tPQz30EyUdXNS
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qcsvas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,16 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+
+
+        //Find Spot
+        app.get('/touristSpot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotCollection.findOne(query);
+            res.send(result);
+        })
+
 
 
         // Send a ping to confirm a successful connection
