df64d6e2cc7209db0eaa50deb1cc4135fffcfda5
Abrarulctg <jubairctg99@gmail.com>
2024-04-30 09:23:25 +0600

    Added delete function

--- index.js
+++ index.js
@@ -54,6 +54,36 @@ async function run() {
         })
 
 
+        //Update coffee
+        // app.put('/coffee/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const filter = { _id: new ObjectId(id) };
+        //     const options = { upsert: true };
+        //     const updatedCoffee = req.body;
+        //     const coffee = {
+        //         $set: {
+        //             name: updatedCoffee.name,
+        //             quantity: updatedCoffee.quantity,
+        //             supplier: updatedCoffee.supplier,
+        //             taste: updatedCoffee.taste,
+        //             category: updatedCoffee.category,
+        //             details: updatedCoffee.details,
+        //             photo: updatedCoffee.photo,
+
+        //         }
+        //     }
+        //     const result = await coffeeCollection.updateOne(filter, coffee, options);
+        //     res.send(result);
+        // })
+
+        //Delete
+        app.delete("/touristSpot/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
